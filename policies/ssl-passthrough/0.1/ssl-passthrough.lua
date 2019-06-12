eff4e9c6a1ff78fb22664aa793a9676e278c1894
Aditya Nugraha <anugraha@redhat.com>
2019-06-12 21:22:44 +0700

    tes

--- policies/ssl-passthrough/0.1/ssl-passthrough.lua
+++ policies/ssl-passthrough/0.1/ssl-passthrough.lua
@@ -10,20 +10,6 @@ end
 function _M:access()
   ngx.log(ngx.INFO, 'SSL Passthrough - _M:access started')
   
-  ngx.log(ngx.INFO, 'SSL Passthrough - ngx.var.ssl_client_raw_cert: ')
-  ngx.log(ngx.INFO, ngx.var.ssl_client_raw_cert)
-  local client_cert = ngx.var.ssl_client_raw_cert and ngx.var.ssl_client_raw_cert:gsub('\\n',' ') or nil
-  
-  ngx.log(ngx.INFO, 'SSL Passthrough - client_cert: ')
-  ngx.log(ngx.INFO, client_cert)
-  ngx.req.set_header("X-SSL-CERT", client_cert)
-  
-  local host = ngx.req.get_headers()["X-SSL-CERT"]
-  --local header-content = ngx.req.get_headers()["X-SSL-CERT"]
-  --local header-content = ngx.header["X-SSL-CERT"]
-  proxy_set_header X-SSL-CERT client_cert
-  ngx.log(ngx.INFO, 'SSL Passthrough - header-content: ')
-  ngx.log(ngx.INFO, host)
   
   ngx.log(ngx.INFO, 'SSL Passthrough - _M:access completed')
 end
@@ -34,7 +20,27 @@ function _M:log()
 end
 
 function _M:rewrite()
-  ngx.log(ngx.INFO, 'SSL Passthrough - Passing through the rewrite phase. - ')
+  ngx.log(ngx.INFO, 'SSL Passthrough - _M:rewrite started ')
+  
+  
+  ngx.log(ngx.INFO, 'SSL Passthrough - ngx.var.ssl_client_raw_cert: ')
+  ngx.log(ngx.INFO, ngx.var.ssl_client_raw_cert)
+  local client_cert = ngx.var.ssl_client_raw_cert and ngx.var.ssl_client_raw_cert:gsub('\\n',' ') or nil
+  
+  ngx.log(ngx.INFO, 'SSL Passthrough - client_cert: ')
+  ngx.log(ngx.INFO, client_cert)
+  ngx.req.set_header("X-SSL-CERT", client_cert)
+  
+  local headerCert = ngx.req.get_headers()["X-SSL-CERT"]
+  --local header-content = ngx.req.get_headers()["X-SSL-CERT"]
+  --local header-content = ngx.header["X-SSL-CERT"]
+  --proxy_set_header X-SSL-CERT $client_cert
+  ngx.log(ngx.INFO, 'SSL Passthrough - headerCert: ')
+  ngx.log(ngx.INFO, headerCert)
+  
+  ngx.log(ngx.INFO, 'SSL Passthrough - _M:rewrite completed ')
+  
+  
 end
 
 function _M:header_filter()
