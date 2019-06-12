f148da10f867af39baf205bdec8a6762076a0941
Aditya Nugraha <anugraha@redhat.com>
2019-06-12 20:47:27 +0700

    tes

--- policies/ssl-passthrough/0.1/ssl-passthrough.lua
+++ policies/ssl-passthrough/0.1/ssl-passthrough.lua
@@ -18,7 +18,7 @@ function _M:access()
   ngx.log(ngx.INFO, client_cert)
   ngx.req.set_header("X-SSL-CERT", client_cert)
   
-  --local header-content = ngx.req.get_headers()["X-SSL-CERT"]
+  local header-content = ngx.req.get_headers()["X-SSL-CERT"]
   --local header-content = ngx.header["X-SSL-CERT"]
   
   ngx.log(ngx.INFO, 'SSL Passthrough - header-content: ')
