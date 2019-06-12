e7f443b4a9304f039b9a4f117ff40b2216a5313c
Aditya Nugraha <anugraha@redhat.com>
2019-06-12 19:44:40 +0700

    log

--- policies/ssl-passthrough/0.1/ssl-passthrough.lua
+++ policies/ssl-passthrough/0.1/ssl-passthrough.lua
@@ -9,9 +9,15 @@ end
 
 function _M:access()
   ngx.log(ngx.INFO, 'SSL Passthrough - _M:access started')
+  
+  ngx.log(ngx.INFO, 'SSL Passthrough - ngx.var.ssl_client_raw_cert: ')
+  ngx.log(ngx.INFO, ngx.var.ssl_client_raw_cert)
   local client_cert = ngx.var.ssl_client_raw_cert and ngx.var.ssl_client_raw_cert:gsub('\\n',' ') or nil
+  
+  ngx.log(ngx.INFO, 'SSL Passthrough - client_cert: ')
+  ngx.log(ngx.INFO, client_cert)
   ngx.req.set_header("X-SSL-CERT", client_cert)
-  ngx.log(ngx.INFO, 'SSL Passthrough - client_cert: ' .. client_cert)
+  
   ngx.log(ngx.INFO, 'SSL Passthrough - _M:access completed')
 end
 
